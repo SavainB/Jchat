aac61dd026587e1408815da386fea8f391338be3
@sjk <sjikayle@gmail.com>
2022-11-06 21:19:51 +0100

    amelioration du coté design des print

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ key_publique_du_mec = ""
 
 
 def inscription():
-    print("Votre pseudo")
-    pseudo = input()
-    print("Votre mot de passe")
-    mdp = input()
+    pseudo = input("| Taper votre pseudo : ")
+    print("+--------------------------------------------+")
+    mdp= input("| Taper votre mot de passe : ")
+    print("+--------------------------------------------+")
     key = (rsa.gen_rsa_keypair(128))
     public_key = (key[0][0], key[0][1])
     conn = mysql.connector.connect(user='c9AB91ZTWs', password='RbvnvmTKqv',
@@ -23,14 +23,14 @@ def inscription():
     conn.commit()
     conn.rollback()
     conn.close()
-    print("vous avez réussi votre inscription")
+    print("| vous avez réussi votre inscription")
 
 
 def connexion():
-    print("Votre pseudo")
-    utilisateur = input()
-    print("Votre Mot de passe")
-    password = input()
+    utilisateur = input("| Taper votre pseudo : ")
+    print("+--------------------------------------------+")
+    password = input("| Taper votre mot de passe : ")
+    print("+--------------------------------------------+")
     conn = mysql.connector.connect(user='c9AB91ZTWs', password='RbvnvmTKqv',
                                    host='remotemysql.com',  port="3306", database='c9AB91ZTWs')
     cursor = conn.cursor()
@@ -44,28 +44,36 @@ def connexion():
                 user_name = name[1]
                 user_id = str(name[0])
                 user_private_key = str(name[3])
-                print(user_private_key)
+                print("| Clé privée ["+user_private_key+"]")
                 conn.commit()
                 conn.rollback()
                 conn.close()
-                print("Bonjour [" + str(user_name) +
+                print("+----------------------------------------------------------+")
+                print("|| Bonjour [" + str(user_name) +
                       "] vous avez réussi votre connexion")
+                print("+----------------------------------------------------------+")
                 return True
-    print("Bonjour votre mot de passe ou nom d'utilisateur est incorrect")
+    print("+----------------------------------------------------------+")
+    print("|| Votre mot de passe ou votre nom d'utilisateur est incorrect")
+    print("+----------------------------------------------------------+")
 
 
 def choix_user():
-    print("Taper l'id de la personne dont vous voulez communiquer")
+    print("+----------------------------------------------------------+")
+    print("| Taper l'id de la personne dont vous voulez communiquer   |")
+    print("+----------------------------------------------------------+")
     conn = mysql.connector.connect(user='c9AB91ZTWs', password='RbvnvmTKqv',
                                    host='remotemysql.com',  port="3306", database='c9AB91ZTWs')
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM `Users`")
     data = cursor.fetchall()
+    print("+----------------------------------------------------------+")
     for name in data:
-        print(str(name[1])+" id ("+str(name[0])+")")
+        print("| "+str(name[1])+" id ["+str(name[0])+"]")
+    print("+----------------------------------------------------------+")
     conn.rollback()
     conn.close()
-    selection = input()
+    selection = input("| Votre séléction : ")
     conn = mysql.connector.connect(user='c9AB91ZTWs', password='RbvnvmTKqv',
                                    host='remotemysql.com',  port="3306", database='c9AB91ZTWs')
     cursor = conn.cursor()
@@ -73,8 +81,10 @@ def choix_user():
     data = cursor.fetchall()
     for name in data:
         if str(name[0]) == str(selection):
+            print("+----------------------------------------------------------+")
             print(
                 "Vous avez choisis l'utilisateur prénommé ["+str(name[1])+"]")
+            print("+----------------------------------------------------------+")
             global id_du_mec, key_publique_du_mec
             id_du_mec = name[0]
             key_publique_du_mec = name[4]
@@ -85,32 +95,41 @@ def choix_user():
 
 
 def state_conversation():
-    print("Affichez la conversation avec cette personne ? taper [1]")
-    print("Envoyez un message à cette personne ? taper [2]")
-    print("Discuter avec quelqu'un d'autre ? taper [3]")
-    choix = input()
+    print("+----------------------------------------------------------+")
+    print("| Affichez la conversation avec cette personne ? Taper [1] |")
+    print("+----------------------------------------------------------+")
+    print("| Envoyez un message à cette personne ?          Taper [2] |")
+    print("+----------------------------------------------------------+")
+    print("| Discuter avec quelqu'un d'autre ?              Taper [3] |")
+    print("+----------------------------------------------------------+")
+    choix = input("| Votre séléction : ")
     if choix == '1':
         conn = mysql.connector.connect(user='c9AB91ZTWs', password='RbvnvmTKqv',
                                        host='remotemysql.com',  port="3306", database='c9AB91ZTWs')
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM `Conversations`")
         data = cursor.fetchall()
+        print("+----------------------------------------------------------+")
         for message in data:
             if str(message[1]) == user_id and str(message[1]) == id_du_mec or str(message[2]) == user_id or str(message[1]) == id_du_mec:
                 msg_chiffrer = eval(message[3])
-                print(msg_chiffrer)
+                
+                print("Le message chiffrer ["+str(msg_chiffrer)+"]")
                 user_private_key = eval(user_online)
                 dec = rsa.rsa_dec(msg_chiffrer, user_private_key)
-                print(dec)
+                print("Le message déchiffrer : "+str(dec))
+        print("+----------------------------------------------------------+")
         conn.commit()
         conn.rollback()
         conn.close()
     elif choix == '2':
         msg = input(str(user_name)+": ")
         while (len(msg) > 17):
+            print("+----------------------------------------------------------+")
             print(
-                "Pour une raison de sécurité, nous avons décider de limiter à au max 16 charactere")
+                "|| Pour une raison de sécurité, nous avons décider de limiter à au max 16 charactere")
             msg = input(str(user_name)+": ")
+            print("+----------------------------------------------------------+")
         enc = rsa.rsa_enc(msg, eval(key_publique_du_mec))
         conn = mysql.connector.connect(user='c9AB91ZTWs', password='RbvnvmTKqv',
                                        host='remotemysql.com',  port="3306", database='c9AB91ZTWs')
@@ -125,9 +144,13 @@ def state_conversation():
 
 
 while (True):
-    print("Voulez-vous vous creer un compte taper [1]")
-    print("Voulez-vous vous connecter taper [2]")
-    choix = input()
+    print("+--------------------------------------------+")
+    print("| Voulez-vous vous creer un compte Taper [1] |")
+    print("+--------------------------------------------+")
+    print("| Voulez-vous vous connecter       Taper [2] |")
+    print("+--------------------------------------------+")
+    choix = input("| Votre séléction : ")
+    print("+--------------------------------------------+")
     if choix == '1':
         inscription()
     elif choix == '2':
